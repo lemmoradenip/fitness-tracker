22b4c88e9260db38334efec7a6b7481bba123af4
lemmor adenip <rmmelpneda.myproject@gmail.com>
2019-05-29 02:31:43 +0800

    basic 3 tab added on trainings

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,41 @@
 //outsource module to manage only necessary modules
 //will be added to app.module.ts
 import { NgModule } from '@angular/core';
-import { MatFormFieldModule, MatIconModule,MatButtonModule,MatInputModule,MatDatepickerModule,MatNativeDateModule,MatSidenavModule,MatToolbarModule, MatListModule } from '@angular/material';
+import {
+  MatFormFieldModule,
+  MatIconModule,
+  MatButtonModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatListModule,
+  MatTabsModule
+} from '@angular/material';
 @NgModule(
-    {
-        imports: [MatFormFieldModule,MatIconModule,MatButtonModule,MatInputModule,MatDatepickerModule,MatNativeDateModule,MatSidenavModule,MatToolbarModule,MatListModule],
-        exports: [MatFormFieldModule,MatIconModule,MatButtonModule,MatInputModule,MatDatepickerModule,MatNativeDateModule,MatSidenavModule,MatToolbarModule,MatListModule]
-    }
+  {
+    imports: [MatFormFieldModule,
+      MatIconModule,
+      MatButtonModule,
+      MatInputModule,
+      MatDatepickerModule,
+      MatNativeDateModule,
+      MatSidenavModule,
+      MatToolbarModule,
+      MatListModule,
+      MatTabsModule],
+    exports: [MatFormFieldModule,
+      MatIconModule,
+      MatButtonModule,
+      MatInputModule,
+      MatDatepickerModule,
+      MatNativeDateModule,
+      MatSidenavModule,
+      MatToolbarModule,
+      MatListModule,
+      MatTabsModule]
+  }
 )
 export class MaterialModule {
 
